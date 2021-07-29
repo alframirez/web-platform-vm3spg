1f710a89cf7f5d9b0ac4cfe07a454562a9318ba3
alframirezg <alframirezg@gmail.com>
2021-07-29 10:55:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-vm3spg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-vm3spg)
---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('hello!');
+
+function MensajeCrear() {
+  alert('Usuario creado');
+}
+function MensajeError() {
+  alert('Usuario y/o Contraseña incorrecta');
+}
+function MensajeCerrarSesion() {
+  alert('¿Esta seguro que desea salir de su sesión?');
+}
+function MensajeGuardarUsuario() {
+  alert('Datos modificados con éxito');
+}
+function MensajeEmergencia() {
+  alert('Nos comunicaremos con usted en la mayor brevedad posible');
+}
+function MensajeCotizacion() {
+  alert('Su solicitud de cotización esta en proceso');
+}
+
+function MensajeEliminar() {
+  alert('¿Esta seguro que desea eliminar a este usuario?');
+  alert('Usuario eliminado');
+}
